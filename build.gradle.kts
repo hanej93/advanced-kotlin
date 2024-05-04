839909dfb74b85900455d1a96c33d02b262d3cf7
EJHan <hanej93@naver.com>
2024-05-05 08:23:24 +0900

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+    id("me.champeau.gradle.jmh") version "0.5.3"
+    id("org.jetbrains.dokka") version "1.9.20"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.reflections:reflections:0.10.2")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.10")
+    testImplementation(kotlin("test"))
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
